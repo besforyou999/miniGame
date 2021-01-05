1c36800ba160abd05911c8d6842ea631cf4e0772
besforyou999 <besforyou2@gmail.com>
2021-01-05 14:50:03 +0900

    chagned a lot

--- test.c
+++ test.c
@@ -1,33 +1,24 @@
-#include<stdio.h>
 #include<ncurses.h>
-#include<stdlib.h>
 #include<unistd.h>
 
-//#define gotoxy(x,y) printf("\033[%d;%dH",(y),(x))
-
-void printPlayer();
-void printEnemy();
-
 int timer(){
-	
+
 	int row = 10, col = 10;
 
 	initscr();
+	noecho();	
 
-	noecho();
-
-	curs_set(FALSE);
+	curs_set(FALSE); 
 
 	keypad(stdscr, TRUE);
 
-	while(1){
-	
+	while(1)
+	{
 		int input = getch();
-
 		clear();
-
+		
 		switch(input)
-		{
+		{	
 			case KEY_UP:
 			mvprintw(--row, col, "A");
 			continue;
@@ -40,33 +31,19 @@ int timer(){
 			case KEY_RIGHT:
 			mvprintw(row, ++col, "A");
 			continue;
-		}
 
-		if(input == 'q') break;
+		}
+			
+		if(input =='q') break;
+
 	}
 
 	endwin();
 
-	return 1;
+	return 0;
 }
 
 int main(){
+
 	return timer();
 }
-
-
-/*
-void printPlayer(){
-	printw("######");
-
-}
-
-
-void printEnemy(){
-
-	move(10,5);
-	printw("------");
-
-}
-
-*/
