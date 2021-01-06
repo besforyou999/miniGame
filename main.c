19d0d9ac3d2c006fedc137cdc6c7feebe108ba91
besforyou999 <besforyou2@gmail.com>
2021-01-06 15:08:34 +0900

    .

--- main.c
+++ main.c
@@ -2,32 +2,37 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <malloc.h>
+#include "linux_kbhit.h"
 
 #define COL_LIMIT 30
 #define HOR_LIMIT 80
 #define BALL_SPEED 3
 
-void printPlayer(int y, int x);
-void drawBox();
-void drawBall(int y, int x);
-void reverse_direction();
-
-typedef struct Ball{
+typedef struct Ball {
 	int x;
 	int y;
 	int direction;
 }BALL;
 
+void printPlayer(int y, int x);
+void drawBox();
+void drawBall(int y, int x);
+void mod_ball_position(BALL * b);
+int control();
+
+int main() {
+	return control();
+}
+
 int control() {
-	
 	
 	int row  = 3, col = COL_LIMIT / 2 - 2 ;
 	int e_posx = HOR_LIMIT - 2, e_posy = COL_LIMIT / 2 - 2;
 
-	BALL ball;	
-	ball.x = HOR_LIMIT / 2 , ball.y = COL_LIMIT / 2;
-
-	
+	BALL *ball=malloc( sizeof(BALL) );	
+	(*ball).x = HOR_LIMIT / 2 , (*ball).y = COL_LIMIT / 2;
+	(*ball).direction = 0;	
 
 	//init curses
 	initscr();
@@ -38,22 +43,22 @@ int control() {
 
 	keypad(stdscr, TRUE);
 
-
-
 	//init game
 	drawBox();
 	printPlayer(col, row);
 	printPlayer(e_posy,e_posx);
-	drawBall(ball_posy,ball_posx);
+	drawBall((*ball).y,(*ball).x);
 
 	mvprintw(COL_LIMIT / 2, HOR_LIMIT / 2 - 10 ,"Press any key to start");
 
-	ball_direction = 0;
-	
-	while(1) {	
-		
-		int input = getch();
+	int input = getch();
 
+	clear();
+
+	while(1) {	
+	
+		input = linux_kbhit();
+		
 		clear();
 
 		//player control
@@ -78,25 +83,20 @@ int control() {
 
 		
 		// ball movement
-		if(ball_posx < 3 || ball_posx > 70) {		
-			reverse_direction();
-		}	 
-	
+		mod_ball_position(ball);
+
 	
 		drawBox();
 		printPlayer(col,row);
 		printPlayer(e_posy, e_posx);
-		drawBall(ball_posy,ball_posx);
+		drawBall((*ball).y,(*ball).x);
 
 	}
 
 	endwin();
 
+	free(ball);
 	return 1;
-}
-
-int main() {
-	return control();
 }
 
 
@@ -155,11 +155,28 @@ void drawBall(int y, int x) {
 	return;
 }
 
+void mod_ball_position(BALL *b) {
 
-void reverse_direction() {
+	if( (*b).x < 3 && (*b).direction == 1 ) {
+			(*b).direction = 0;
+	} 
+	else if( (*b).x > 75 && (*b).direction == 0 ) {
+			(*b).direction = 1;
+	}
 
-		
 
+	if ( (*b).direction == 0 ) {
+			(*b).x += 1;
+
+	} else if ( (*b).direction == 1 ) {
+			(*b).x -= 1;
+	}
+
+	return;
 }
+
+
+
+
 
 
